d5d9fa916e8f43e67944c5c629ef4cbc94b40249
Tamim Hasan Saad <tamimsaad1812@gmail.com>
2024-08-31 21:49:40 +0600

    initial commit

---
+++ Target Sum.cpp
@@ -0,0 +1,21 @@
+// say Alhamdulillah
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        
+    }
+};
+
+// int main() {
+//   Solution s;
+//   vector<int> nums; //= {1, 2, 3, 4};
+//   int x;
+//   while (cin >> x) {
+//     nums.push_back(x);
+//   }
+//   cout << s.canPartition(nums);
+// }
