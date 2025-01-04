73b603188626fb27757b18d4fc533a2d762d78da
Tamim Hasan Saad <tamimsaad1812@gmail.com>
2025-01-04 14:03:06 +0600

    Alhamdulillah

--- Find Minimum in Rotated Sorted Array.cpp
+++ Find Minimum in Rotated Sorted Array.cpp
@@ -6,27 +6,16 @@ using namespace std;
 class Solution {
 public:
   int findMin(vector<int> &nums) {
-    int n = nums.size();
-    int l = 0;
-    int h = n - 1;
-    int ans, m;
-
-    while (l <= h) {
-      if (nums[l] < nums[h]) {
-        ans = nums[l];
-        return ans;
-      }
+    int l = 0, m;
+    int h = nums.size() - 1;
+    while (l < h) {
       m = (l + h) / 2;
-      ans = nums[m];
 
-      // cout<<nums[m]<<endl;
-
-      if (nums[l] <= nums[m]) {
+      if (nums[m] > nums[h])
         l = m + 1;
-      } else {
+      else
         h = m;
-      }
     }
-    return ans;
+    return nums[l];
   }
 };
