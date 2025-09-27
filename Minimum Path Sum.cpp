84b9482165cfc97f8605ac124894d706ca7698df
Tamim Hasan Saad <tamimsaad1812@gmail.com>
2025-09-27 12:02:38 +0600

    Add implementation for Minimum Path Sum problem

---
+++ Minimum Path Sum.cpp
@@ -0,0 +1,33 @@
+/// Alhamdulillah
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  int minPathSum(vector<vector<int>> &grid) {
+    int m = grid.size();
+    int n = grid[0].size();
+
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+
+        if (!i && !j)
+          continue;
+
+        if (!i) {
+          grid[i][j] += grid[i][j - 1];
+          continue;
+        }
+        if (!j) {
+          grid[i][j] += grid[i - 1][j];
+          continue;
+        }
+
+        grid[i][j] += min(grid[i - 1][j], grid[i][j - 1]);
+      }
+    }
+
+    return grid[m - 1][n - 1];
+  }
+};
